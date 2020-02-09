ddd32b38992b897f550f8917df15418a17dfe41a
tboyd4 <tboyd1898@gmail.com>
2020-02-09 11:23:39 -0600

    adding html and css pages to hold my javascript

--- index.js
+++ index.js
@@ -15,5 +15,16 @@ inquirer
 
         axios.get(queryUrl).then((res) => {
             console.log(res.data);
+
+            // pulling specific data out
+            let profImage = res.data.avatar_url;
+            let userName = res.data.login;
+            let userLoca = res.data.location;
+            let userGitHubProf = res.data.url;
+            let userBlog = res.data.blog;
+            let userBio = res.data.bio;
+            let pubRepo = res.data.public_repos;
+            let followers = res.data.followers;
+            let following = res.data.following;
         })
     })
